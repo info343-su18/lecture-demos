2a10c81262ca6ab92a71a211820defa46cf4744a
Joel Ross <joelross@uw.edu>
2018-07-09 11:37:47 -0700

    End of react state demo

--- react-interactive/src/App.js
+++ react-interactive/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import 'whatwg-fetch';
+
 const SAMPLE_TASKS = [
   {id:1, description:'Learn JSX', complete:true},
   {id:2, description:'Learn about React State', complete:false},
@@ -8,11 +10,61 @@ const SAMPLE_TASKS = [
 
 
 class App extends Component {
+  constructor(props){
+    super(props); //pass up to parent
+
+    //initialize state
+    this.state = {
+      tasks: [] //SAMPLE_TASKS //store the tasks in the STATE, initialize
+    };
+
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/repos/info343-su18/lecture-demos/issues')
+      .then((res) => res.json())
+      .then((data) => {
+        let issueTasks = data.map((issue) => {
+          return {
+            id: issue.id,
+            description: issue.title,
+            complete: false
+          }
+        })
+        this.setState({tasks:issueTasks})        
+      })
+  }
+
+  toggleComplete(taskId){
+    let updatedTasks = this.state.tasks.map((task) => {
+      if(task.id === taskId)
+        task.complete = !task.complete;      
+      return task;
+    })
+
+    this.setState({tasks: updatedTasks}); //update the state and RE-RENDER
+  }
+
   render() {
+
+    let incomplete = this.state.tasks.filter((task) => !task.complete);
+    console.log(incomplete.length);
+
+    //CONDITIONAL RENDERING
+    let listToShow = null;
+    if(incomplete.length !== 0){ //nothing to do
+      listToShow = <TaskList 
+        tasks={this.state.tasks} 
+        howToToggle={(id) => this.toggleComplete(id)} 
+      />
+    } else {
+      listToShow = <p className="alert-alert-warning">Downloading...</p>
+    }
+
     return (
       <div className="container">
-        <p className="lead">Things I have to do (2+)</p>
-        <TaskList />
+        <p className="lead">Things I have to do ({incomplete.length})</p>
+        {listToShow}
         <AddTaskForm />
       </div>
     );
@@ -20,21 +72,66 @@ class App extends Component {
 }
 
 class TaskList extends Component {  
-  render() {   
+  render() {
+    
+    let taskComponents = this.props.tasks
+      .filter((task) => !task.complete)
+      .map((eachTask) => {
+        return (
+          <Task 
+            key={eachTask.id} 
+            task={eachTask} 
+            howToToggle={this.props.howToToggle}
+          />
+        ); //a thing in the array
+      })
+
     return (
       <ol>
-        <Task />
-        {/* tasks go here */}
+        {taskComponents}
       </ol>
     );
   }
 }
 
 class Task extends Component {
+  getClassName() {
+    let className = '';
+    if(this.props.task.complete){
+      className = 'font-strike';
+    }
+    return className;    
+  }
+
+  //change this in a moment
+  handleClick() {
+    this.props.howToToggle(this.props.task.id);
+
+    // //want: this.props.task.complete = false
+    // console.log("You clicked on '"+this.props.task.description+"'");
+    // let newCrossed = !this.state.crossedOut;
+    // this.setState({crossedOut: newCrossed}); //update the state and RE-RENDER
+    // console.log("in handler", this.state);
+  }
+
+  componentDidMount() {
+    console.log("mounting", this.props.task.description);
+  }
+
+  componentWillUnmount() {
+    console.log("unmounting", this.props.task.description);
+  }
+
   render() {
+    //console.log(this.props.task);    
+    let thisTask = this.props.task;
+
     return (
-      <li>
-        task description
+      <li 
+        className={this.getClassName()} 
+        onClick={() => this.handleClick()}
+        >
+          {thisTask.description}
       </li>
     );
   }
